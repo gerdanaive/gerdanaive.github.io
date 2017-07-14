93b590f7dde37b58951fd6af82575f05a1b21bf2
Oksana Stepanenko <gerdanaive@gmail.com>
2017-07-14 11:50:12 +0300

    Update site

--- js/app.js
+++ js/app.js
@@ -1,2 +1,12 @@
-"use strict";document.addEventListener("click",function(e){var t=document.getElementsByClassName("page-nav__toggle")[0];e.target.closest(".page-nav__list")||e.target===t||e.target.parentNode===t||document.getElementsByClassName("page-nav__list")[0].classList.remove("show")}),document.getElementsByClassName("page-nav__toggle")[0].addEventListener("click",function(e){document.getElementsByClassName("page-nav__list")[0].classList.toggle("show")});
+"use strict";
+
+document.addEventListener("click", function (e) {
+  var toggle = document.getElementsByClassName("page-nav__toggle")[0];
+  if (!e.target.closest('.page-nav__list') && e.target !== toggle && e.target.parentNode !== toggle) {
+    document.getElementsByClassName("page-nav__list")[0].classList.remove('show');
+  }
+});
+document.getElementsByClassName("page-nav__toggle")[0].addEventListener("click", function (e) {
+  document.getElementsByClassName("page-nav__list")[0].classList.toggle("show");
+});
 //# sourceMappingURL=../js/app.js.map
